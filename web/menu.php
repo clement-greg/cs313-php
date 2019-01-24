dc506b69c6efe73233d01809fff3e49f0bb295f5
Greg Clement <gregbclement@byui.edu>
2019-01-24 15:51:23 -0700

    updated menu

--- web/menu.php
+++ web/menu.php
@@ -17,7 +17,16 @@
     <div class="center">
         <img src="/images/menu-icon.jpg" class="menu-icon">
         <h1>CS 313 Menu</h1>
-
+        <a href="/index.php" class="menu-item">
+            <div class="flex-container">
+                <div class="flex-none"><i class="material-icons">chevron_right
+                    </i></div>
+                <div>
+                    <h3>Week 02 - Prove</h3>
+                    <p>Home Page</p>
+                </div>
+            </div>
+        </a>
         <a href="/week03_team.php" class="menu-item">
             <div class="flex-container">
                 <div class="flex-none"><i class="material-icons">chevron_right
